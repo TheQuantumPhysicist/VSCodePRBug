f9e3091f8f07ac386ce1627fda551e4b46632a6a
Samer Afach <info@afach.de>
2022-10-25 16:26:21 +0400

    Do stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn do_stuff() -> String {
+    let s1 = "Look at me doing some stuff... ";
+    let s2 = "and... doing even more stuff";
+    String::new() + s1 + s2
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", do_stuff());
 }
